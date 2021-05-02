f993375e0783534dabe6eaf8dc14c8759d5f624a
Antonio A <andrade.antonio@gmail.com>
2021-05-02 16:08:46 +0400

    updates

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 import argparse
+import sys
 import traceback
 from getpass import getpass
 
 from loguru import logger
 
-import radius
+from radius import Attributes, ChallengeResponse, authenticate
 
 
 def main(username, password, secret, host, port=1812):
+    def _status(outcome):
+        if outcome:
+            print("Authentication Succeeded")
+            sys.exit(0)
+        else:
+            sys.exit("Authentication Failed")
+
+    err = None
 
     try:
-        r = radius.authenticate(
-            secret=secret, username=username, password=password, host=host, port=port
-        )
-        logger.tracer(type(r))
-        logger.tracer(r)
-        logger.tracer(r.__dict__)
-        logger.tracer(r.__class__)
+        _status(authenticate(secret, username, password, host=host, port=port))
+    except ChallengeResponse as e:
+        err = e
+    except Exception:
+        traceback.print_exc()
+        sys.exit("Authentication Error")
 
-    except radius.ChallengeResponse as e:
-        logger.info(f"Successfully authenticated. State={e.state}")
-    except Exception as e:
-        logger.error(f"An error occurried: {e}")
-    except:
-        logger.error(traceback.print_exc)
+    print("RADIUS server replied with a challenge.")
+
+    for m in getattr(err, "messages", []):
+        print(" - %s" % m)
+
+    response = None
+    while not response:
+        response = getpass("Enter your challenge response: ")
+
+    state = getattr(err, "state", None)
+    a = Attributes({"State": state} if state else {})
+
+    try:
+        _status(
+            authenticate(secret, username, response, host=host, port=port, attributes=a)
+        )
+    except Exception:
+        traceback.print_exc()
+        sys.exit("Authentication Error")
 
 
 if __name__ == "__main__":
