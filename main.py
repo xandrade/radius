c944b624604a543374a7a303fb56fdeb2e3a4ccd
Antonio A <andrade.antonio@gmail.com>
2021-05-02 11:15:47 +0400

    update

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
-import radius
-from loguru import logger
 import argparse
 
+from loguru import logger
 
-def main(user, pwd, secret, host, port):
+import radius
 
-  r = radius.Radius(secret, host=host, port=port)
 
-  try:
-      logger.info('successfully authenticated' if r.authenticate(username, password) else 'failure')
-      sys.exit(0)
-  except radius.ChallengeResponse as e:
-      logger.error(f'An error occurred: {e}')
+def main(username, password, secret, host, port):
 
-  # The ChallengeResponse exception has `messages` and `state` attributes
-  # `messages` can be displayed to the user to prompt them for their
-  # challenge response. `state` must be echoed back as a RADIUS attribute.
+    try:
+        r = radius.authenticate(secret, username, password, host=host, port=port)
+        logger.tracer(type(r))
+        logger.tracer(r)
+        logger.tracer(r.__dict__)
+        logger.tracer(r.__class__)
 
-  # Send state as an attribute _IF_ provided.
-  attrs = {'State': e.state} if e.state else {}
+    except Exception as e:
+        logger.error(f"An error occurried: {e}")
 
-  # Finally authenticate again using the challenge response from the user
-  # in place of the password.
-  logger.info('success' if r.authenticate(username, response, attributes=attrs)
-                  else 'failure')
 
-  
-  # Another way
-
-  r = radius.authenticate(secret, username, password, host=host, port=port)
-  logger.tracer(r)
-  
-  
 if __name__ == "__main__":
-  
-  parser = argparse.ArgumentParser(description='RADIUS Challenge/Response Authentication')
-  parser.add_argument('user', type=str, help='user id (e.g. MU59145)')
-  parser.add_argument('pwd', type=str, help='user password')
-  parser.add_argument('secret', type=str, help='RADIUS Secret')
-  parser.add_argument('host', type=str, help='RADIUS server hostname/IP')
-  parser.add_argument('port', type=int, help='RADIUS server port', default=1812)
 
-  args = parser.parse_args()
-  user = args.user
-  pwd = args.pwd
-  secret = args.secret
-  host = args.host
-  
-  main(user, pwd, secret, host)
+    parser = argparse.ArgumentParser(
+        description="RADIUS Challenge/Response Authentication"
+    )
+    parser.add_argument("username", type=str, help="user id (e.g. MU59145)")
+    parser.add_argument("password", type=str, help="user password")
+    parser.add_argument("secret", type=str, help="RADIUS Secret")
+    parser.add_argument("host", type=str, help="RADIUS server hostname/IP")
+    parser.add_argument("port", type=int, help="RADIUS server port", default=1812)
+
+    args = parser.parse_args()
+    username = args.username
+    password = args.password
+    secret = args.secret
+    host = args.host
+    port = args.port
+
+    main(username, password, secret, host, port)
